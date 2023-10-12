d1762507a9774887da60ede64593fa11287968a2
kaylaaditya <kaylaaditya123@outlook.com>
2023-10-12 08:14:44 +0700

    tambah file show.blade

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Show</title>
+</head>
+
+<body style="background: lightgray">
+    <div class="container mt-5 mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <img src="{{ Storage::url('public/posts/').$post->image }}" alt="w-100 rounded">
+                        <hr>
+                        <h4>{{ $post->title }}</h4>
+                        <p class="tmt-3">
+                            {!! $post->content !!}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/bootstrap.min.js"></script>
+</body>
+
+</html>
